ed70d37e5d41e4cdaba63c18653b27d5dc69c951
Swati Muduli <swatimuduli95@gmail.com>
2026-07-21 20:29:46 +0530

    Time: 0 ms (100%), Space: 44.4 MB (39.53%) - LeetHub - Time: O(n + m), Space: O(1)

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -11,26 +11,29 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode dummy=new ListNode(-1);
-        ListNode current=dummy;
+        ListNode tail=dummy;
         while(list1!=null && list2!=null){
-            if(list1.val<list2.val){
-                current.next=list1;
-                list1=list1.next;
-            }
-            else{
-                current.next=list2;
+            if(list1.val>=list2.val){
+                tail.next=list2;
                 list2=list2.next;
             }
-            current=current.next;
+            else{
+                tail.next=list1;
+                list1=list1.next;
+            }
+            tail=tail.next;
         }
         if(list1!=null){
-            current.next=list1;
-
+            tail.next=list1;
         }
         else{
-            current.next=list2;
+            tail.next=list2;
         }
         return dummy.next;
         
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
