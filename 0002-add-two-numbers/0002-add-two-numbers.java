1e0c665369d91378e13acc03444f3335943daecd
Swati Muduli <swatimuduli95@gmail.com>
2026-07-23 20:21:33 +0530

    Time: 1 ms (100%), Space: 46.6 MB (19.64%) - LeetHub - Time: O(max(m, n)), Space: O(max(m, n))

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,30 +1,31 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        // 1. Dummy node aur carry initialize kiya 🧱
-        ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
+        ListNode result = new ListNode(-1);
+        ListNode ptr = result;
         int carry = 0;
-        
-        // 2. Loop tab tak chalega jab tak l1, l2 ya carry mein se kuch bhi bacha ho 🔄
-        while (l1 != null || l2 != null || carry != 0) {
-            int sum = carry; // Pehle carry add karo ➕
-            
+
+        while (l1 != null || l2 != null) {
+            int sum = 0 + carry;
             if (l1 != null) {
-                sum += l1.val;
+                sum = sum + l1.val;
                 l1 = l1.next;
             }
-            
             if (l2 != null) {
-                sum += l2.val;
+                sum = sum + l2.val;
                 l2 = l2.next;
             }
-            
-            // 3. Naya carry aur value calculate karo 🧮
             carry = sum / 10;
-            current.next = new ListNode(sum % 10);
-            current = current.next;
+            sum = sum % 10;
+            ptr.next = new ListNode(sum);
+            ptr = ptr.next;
         }
-        
-        return dummy.next;
+        if (carry == 1) {
+            ptr.next = new ListNode(carry);
+        }
+        return result.next;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
