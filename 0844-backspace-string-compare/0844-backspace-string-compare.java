4e49ddc35537d6060da35f4c9785ead66034251a
Swati Muduli <swatimuduli95@gmail.com>
2026-08-05 10:44:32 +0530

    Time: 1 ms (81.83%), Space: 42.6 MB (97.69%) - LeetHub - Time: O(n + m), Space: O(n + m)

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,23 +1,32 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-       String finalS=process(s);
-       String finalT=process(t);
-       return finalS.equals(finalT); 
+        String sb1=neww(s);
+        String sb2=neww(t);
+    
+     return sb1.equals(sb2);
+
+        
     }
-    private String process(String str){
-        StringBuilder sb=new StringBuilder();
-        for(int i=0;i<str.length();i++){
-            char c=str.charAt(i);
+    public String neww(String s){
+       StringBuilder sb = new StringBuilder();
+        for(int i=0;i<s.length();i++ ){
+            char c=s.charAt(i);
             if(c=='#'){
-                if(sb.length()>0){
-                    sb.deleteCharAt(sb.length()-1);
+               if(sb.length() > 0){
+                    sb.deleteCharAt(sb.length() - 1);
                 }
             }
             else{
                 sb.append(c);
+
             }
+
         }
         return sb.toString();
-        
+
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
