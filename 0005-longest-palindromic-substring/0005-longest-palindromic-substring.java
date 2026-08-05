3d67de8dd1f098dfabe25997d12cc91653ad6de9
Swati Muduli <swatimuduli95@gmail.com>
2026-08-05 21:27:20 +0530

    Time: 2575 ms (5.07%), Space: 47 MB (16.99%) - LeetHub - Time: O(n^3), Space: O(n)

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        String result = "";
+        
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                String sub = s.substring(i, j + 1);
+                if (isPalindrome(sub) && sub.length() > result.length()) {
+                    result = sub;
+                }
+            }
+        }
+        
+        return result;
+    }
+    
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
