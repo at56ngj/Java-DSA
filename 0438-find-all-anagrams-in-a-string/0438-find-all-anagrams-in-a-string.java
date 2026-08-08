82ce91ab4ebea3ac54e0e24d49a5a19250f4a856
Swati Muduli <swatimuduli95@gmail.com>
2026-08-08 17:30:16 +0530

    Time: 1797 ms (5.05%), Space: 47.8 MB (12.56%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,24 +1,25 @@
-import java.util.*;
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-       List<Integer> list=new ArrayList<>();
-       int n=p.length();
-       char parr[]=p.toCharArray();
-       Arrays.sort(parr);
-       String psorted=new String(parr);
-       for(int i=0;i<=s.length()-n;i++){
-        String sub=s.substring(i,i+n);
-        char subarr[]=sub.toCharArray();
-        Arrays.sort(subarr);
-        String subsorted=new String(subarr);
-        if(subsorted.equals(psorted)){
-            list.add(i);
-        }
-       }
-       return list;
+        List <Integer> list=new ArrayList<>();
+        char[] parr=p.toCharArray();
+        Arrays.sort(parr);
+        String psorted=new String(parr);
+        int n=p.length();
+        for(int i=0;i<=s.length()-n;i++){
+            String sub=s.substring(i,i+n);
+            char[] sarr=sub.toCharArray();
+            Arrays.sort(sarr);
+            String ssorted=new String(sarr);
 
+            if(psorted.equals(ssorted)){
+                list.add(i);
+            }
+
+        }
+        return list;
+        
     }
-} 
+}
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
