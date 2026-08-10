ed3988fa312cdc9168ca8d75dab8ccf6a706a7db
Swati Muduli <swatimuduli95@gmail.com>
2026-08-10 21:17:11 +0530

    Time: 0 ms (100%), Space: 48.6 MB (14.31%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left=0;
+        int right=nums.length-1;
+       while(left<=right){
+            int mid=(left+right)/2;
+            if(target<nums[mid]){
+                right=mid-1;
+                
+            }
+            else if(target>nums[mid]){
+                left=mid+1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
