42cb8afab58ce6f90a1982185687752d085e0735
Swati Muduli <swatimuduli95@gmail.com>
2026-08-15 19:17:57 +0530

    Time: 30 ms (95.93%), Space: 75.7 MB (33.35%) - LeetHub - Time: O(n log n), Space: O(1)

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        return nums[n-k];
+
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
