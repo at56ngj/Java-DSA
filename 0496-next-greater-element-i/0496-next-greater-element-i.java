553e6ffa10841a5bfa043cf1aece1b2a8ea63915
Swati Muduli <swatimuduli95@gmail.com>
2026-08-31 10:31:42 +0530

    Time: 5 ms (14.42%), Space: 44.6 MB (95.07%) - LeetHub - Time: O(nums1.length * nums2.length), Space: O(nums1.length + nums2.length)

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,26 +1,33 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int ans[]=new int[nums1.length];
-        for(int i=0;i<nums1.length;i++){
-           int index=-1;
-           for(int j=0;j<nums2.length;j++){
+        int arr[]=new int[nums1.length];
+        int arr2[]=new int[nums2.length];
+        Stack<Integer> stack=new Stack<>();
+        for(int i=nums2.length-1;i>=0;i--){
+            while(!stack.isEmpty()&&nums2[i]>=stack.peek()){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                arr2[i]=-1;
+            }else{
+                arr2[i]=stack.peek();//ans ko array main daalo
+            }
+             stack.push(nums2[i]);//current ko stack main  push karo
+        }
+      
+      for(int i=0;i<nums1.length;i++){
+        for(int j=0;j<nums2.length;j++){
             if(nums1[i]==nums2[j]){
-                index=j;
-                break;
-            }
-           }
-           
-        
-        ans[i]=-1;
-        for(int p=index+1;p<nums2.length;p++){
-            if(nums2[index]<nums2[p]){
-                ans[i]=nums2[p];
+                arr[i]=arr2[j];
                 break;
             }
         }
-
-        }
-        return ans;
+      }
+      return arr;
         
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
