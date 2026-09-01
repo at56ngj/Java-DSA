176271ca58854db138e39cc67d820486e27cbc9e
Swati Muduli <swatimuduli95@gmail.com>
2026-09-01 09:31:01 +0530

    Time: 17 ms (23.12%), Space: 48.3 MB (69.18%) - LeetHub - Time: O(n), Space: O(n)

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,18 +1,25 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int n=nums.length;
-        int ans[]=new int[n];
-        for(int i=0;i<n;i++){
-            ans[i]=-1;
-            for(int j=1;j<n;j++){
-                int nextIndex=(i+j)%n;
-                if(nums[nextIndex]>nums[i]){
-                    ans[i]=nums[nextIndex];
-                    break;
-                }
+        int arr[]=new int[nums.length];
+        Stack<Integer> stack=new Stack<>();
+        for(int i=2*nums.length-1;i>=0;i--){
+            int index=i%nums.length;
+            while(!stack.isEmpty() && nums[index]>=stack.peek()){
+                stack.pop();
             }
+            if(stack.isEmpty()){
+                arr[index]=-1;
+            }else{
+                arr[index]=stack.peek();
+            }
+            stack.push(nums[index]);
         }
-        return ans;
+        return arr;
+
         
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
